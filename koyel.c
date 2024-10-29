fc93f26ad022b0e1854c0c9219c431b797306b98
koyel001 <dchumki655@gmail.com>
2024-10-29 22:44:17 +0530

    Update

---
+++ koyel.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int marks;
+
+    printf("Enter the marks (0-100): ");
+    scanf("%d", &marks);
+
+    if (marks >= 90 && marks <= 99) {
+        printf("Grade: A\n");
+    } else if (marks >= 70 && marks <= 89) {
+        printf("Grade: B\n");
+    } else if (marks >= 50 && marks <= 69) {
+        printf("Grade: C\n");
+    } else if (marks >= 25 && marks <= 49) {
+        printf("Grade: D\n");
+    } else if (marks >= 0 && marks <= 24) {
+        printf("Grade: Failed\n");
+    } else {
+        printf("Invalid marks\n");
+    }
+
+    return 0;
+}
