c60292da2c5c5336d156e86e9f0fa3aea409eab9
koyel001 <dchumki655@gmail.com>
2024-11-13 22:53:16 +0530

    commit

---
+++ Array/Substract_Array.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main(void){
+    int a[2][2],b[2][2];
+    int sub[2][2];
+    
+    //Taking input from matrix a
+    printf("Enter the value for matrix a \n");
+    for(int i=0; i<2; i++){
+        for(int j=0; j<2; j++){
+            printf("Matrix [%d][%d] :",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+    //Taking input from matrix b
+    printf("Enter the value for matrix b \n");
+    for(int i=0; i<2; i++){
+        for(int j=0; j<2; j++){
+            printf("Matrix [%d][%d] :",i,j);
+            scanf("%d",&b[i][j]);
+        }
+    }
+    //Substraction of two Array
+    for(int i=0; i<2; i++){
+        for(int j=0; j<2; j++){
+            sub[i][j]= a[i][j] - b[i][j];
+        }
+    }
+    //printing the substraction
+    printf("The Substraction of Matrix a and b : \n");
+    for(int i=0; i<2; i++){
+        for(int j=0; j<2; j++){
+            printf("%d ", sub[i][j]);
+        }
+        printf("\n");
+    }
+
+return 0;
+
+}
